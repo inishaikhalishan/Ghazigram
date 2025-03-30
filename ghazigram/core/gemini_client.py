42e542c3159b34335be298d718fa5d5dcb50138d
inishaikhalishan <inishaikhalishan@gmail.com>
2025-03-31 02:04:35 +0530

    google-generativeai Library Error handle

--- ghazigram/core/gemini_client.py
+++ ghazigram/core/gemini_client.py
@@ -1,15 +1,19 @@
-import google.generativeai as genai
 from dotenv import load_dotenv
 from os import environ
 
+gemini = None
+
+try:
+    import google.generativeai as genai
+
+    load_dotenv()
 
 
-load_dotenv()
+    api_key = environ["gemini_api"]
 
 
-api_key = environ["gemini_api"]
+    genai.configure(api_key=api_key)
 
-
-genai.configure(api_key=api_key)
-
-gemini = genai.GenerativeModel("gemini-2.0-flash").start_chat()
+    gemini = genai.GenerativeModel("gemini-2.0-flash").start_chat()
+except Exception:
+        print("google-generativeai library is not installled.")
